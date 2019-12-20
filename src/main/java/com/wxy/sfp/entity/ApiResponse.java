2ce4b79a26bc470eaa10f9645b7e425f08e5713f
wangxiaoyuan <243548880@qq.com>
2019-12-20 13:43:32 +0800

    Revert "优化"
    This reverts commit 63a1aa2f

--- src/main/java/com/wxy/sfp/entity/ApiResponse.java
+++ src/main/java/com/wxy/sfp/entity/ApiResponse.java
@@ -7,10 +7,11 @@ import lombok.Data;
 /**
  * @Author wxy
  * @Date 19-7-17 下午3:29
- * @Description TODO 请求响应数据
+ * @Description TODO
  **/
 @Data
 @AllArgsConstructor
+@Builder
 public class ApiResponse {
     private Integer code;
     private String msg;
--- src/main/java/com/wxy/sfp/entity/FileInfo.java
+++ src/main/java/com/wxy/sfp/entity/FileInfo.java
@@ -6,11 +6,11 @@ import lombok.Data;
 /**
  * @Author wxy
  * @Date 19-7-17 上午10:56
- * @Description TODO 文件视图类
+ * @Description TODO
  **/
 @Data
 @Builder
-public class FileInfoVo {
+public class FileInfo {
     private String name;
     private String path;
     private Boolean isDirectory;
